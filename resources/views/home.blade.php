d8ffe895605f526db59c2850b303170628027afb
Damian Klisiewicz <damianklisiewicz@gmail.com>
2021-01-09 01:14:13 +0000

    update homepage with order search form

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,16 +5,20 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
+                <div class="card-header">Check your order status</div>
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
                     @endif
-
-                    {{ __('You are logged in!') }}
+                        <form action="/order" method="get">
+                            <div class="form-group">
+                                <label for="orderNumber">Order number</label>
+                                <input type="text" name="orderNumber" id="orderNumber" class="form-control" required>
+                            </div>
+                            <button class="btn btn-primary">Search</button>
+                        </form>
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,5 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
-Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
